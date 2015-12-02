9687092cb59f778107a3643464b2a85e78ca125a
gaetjen <johannesgaetjen@gmail.com>
2015-12-02 16:47:55 +0100

    some slight code changes

--- ex6/src/tuningCurve.m
+++ ex6/src/tuningCurve.m
@@ -1,11 +1,16 @@
 %authors: Johannes Gätjen, Maria del Cerro, Lorena Morton
-rads = (0:11)*pi/12;
+%maximum 1: 30°
+%maximum 2: 60°
+num_deg_samples = 12;
+rads = (0:num_deg_samples-1)*pi/num_deg_samples;
+degs = rads ./ pi * 180;
 stimulusSize = 50;
 responses1_m_std = [rads * 0; rads*0];
 responses2_m_std = [rads * 0; rads*0];
-repititions = 5000;
+repititions = stimulusSize;
 s_types = [1,2];
 
+figure('Position', [50, 50, 800, 600], 'Name', 'Left: On Bar, Right: Off Bar');
 for s = s_types
     for i = 1:length(rads)
         display(i);
@@ -25,18 +30,28 @@ for s = s_types
         responses2_m_std(1, i) = mean(rep_response2);
         responses2_m_std(2, i) = std(rep_response2);
     end
-    
     if s == 1
-        figure('Position', [50, 50, 400, 600], 'Name', 'On Bar');
+        subplot(2, 2, 1);
     else
-        figure('Position', [50, 50, 400, 600], 'Name', 'Off Bar');
+        subplot(2, 2, 2);
     end
-    subplot(2, 1, 1);
-    plot(rads, responses1_m_std(1,:));hold on;
-    plot(rads, responses1_m_std(1,:) + responses1_m_std(2,:), '--r');
-    plot(rads, responses1_m_std(1,:) - responses1_m_std(2,:), '--r');
-    subplot(2, 1, 2);
-    plot(rads, responses2_m_std(1,:));hold on;
-    plot(rads, responses2_m_std(1,:) + responses2_m_std(2,:), '--r');
-    plot(rads, responses2_m_std(1,:) - responses2_m_std(2,:), '--r');
+    plot(degs, responses1_m_std(1,:));hold on;
+    plot(degs, responses1_m_std(1,:) + responses1_m_std(2,:), '--r');
+    plot(degs, responses1_m_std(1,:) - responses1_m_std(2,:), '--r');
+    xlabel('bar angle [$^\circ$]', 'interpreter', 'latex', 'fontsize', 16);
+    ylabel('firing rate [Hz]', 'interpreter', 'latex', 'fontsize', 16);
+    l=legend('$\mu$', '$\mu \pm \sigma$'); 
+    set(l, 'interpreter', 'latex', 'Fontsize', 12)
+    if s == 1
+        subplot(2, 2, 3);
+    else
+        subplot(2, 2, 4);
+    end
+    plot(degs, responses2_m_std(1,:));hold on;
+    plot(degs, responses2_m_std(1,:) + responses2_m_std(2,:), '--r');
+    plot(degs, responses2_m_std(1,:) - responses2_m_std(2,:), '--r');
+    xlabel('bar angle [$^\circ$]', 'interpreter', 'latex', 'fontsize', 16);
+    ylabel('firing rate [Hz]', 'interpreter', 'latex', 'fontsize', 16);
+    l=legend('$\mu$', '$\mu \pm \sigma$'); 
+    set(l, 'interpreter', 'latex', 'Fontsize', 12)
 end
